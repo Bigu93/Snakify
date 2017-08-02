2b287d83ada1991bacf00fb070f8ad60da54d21b
Marcin Szymanek <marcinszymanek93@gmail.com>
2017-08-02 20:23:54 +0200

    Change bishop_moves

--- Conditions/bishop_moves.py
+++ Conditions/bishop_moves.py
@@ -3,11 +3,7 @@ b1 = int(input())
 a2 = int(input())
 b2 = int(input())
 
-<<<<<<< HEAD
 if abs(a1 - a2) == abs(b1 - b2):
-=======
-if (a1 + b1) % 2 == 0 and (a2 + b2) % 2 == 0:
->>>>>>> master
     print("YES")
 else:
     print("NO")
