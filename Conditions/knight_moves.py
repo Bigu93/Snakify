cf09b6d2881767e621c6f0c22aee0c90728a68d3
Marcin Szymanek <marcinszymanek93@gmail.com>
2017-08-02 20:32:59 +0200

    Knight moves

---
+++ Conditions/knight_moves.py
@@ -0,0 +1,10 @@
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+
+if ((abs(a1 - a2) == 2 and abs(b1 - b2) == 1) or
+        (abs(a1 - a2) == 1 and abs(b1 - b2) == 2)):
+    print("YES")
+else:
+    print("NO")
