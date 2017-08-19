3ce479eba2cbedec01ecedd0d043273c934a5427
Marcin Szymanek <marcinszymanek93@gmail.com>
2017-08-19 13:03:53 +0200

    Slices

---
+++ Strings/slices.py
@@ -0,0 +1,12 @@
+string = input()
+
+print(string[2],
+      string[-2],
+      string[:5],
+      string[:-2],
+      string[::2],
+      string[1::2],
+      string[::-1],
+      string[-1::-2],
+      len(string),
+      sep="\n")
